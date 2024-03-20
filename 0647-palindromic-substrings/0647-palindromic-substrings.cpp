5b6f6157e4fedd467954da9eb25710bb7a4eef37
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-03-20 15:52:06 +0530

    Time: 385 ms (17.38%), Space: 7.6 MB (64.84%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-     int dp[1001][1001];
     int fun(string &s,int i,int j)
     {
         if(i>=j)
         {
             return 1;
         }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
         if(s[i]==s[j])
         {
-            return dp[i][j]=fun(s,i+1,j-1);
+            return fun(s,i+1,j-1);
         }
-        return dp[i][j]=0;
+        return 0;
     }
     int countSubstrings(string s) {
         int ans=0;
         int maxlen=INT_MIN;
         int n=s.length();
-        memset(dp,-1,sizeof(dp));
         int st=0;
         for(int i=0;i<n;i++)
         {
