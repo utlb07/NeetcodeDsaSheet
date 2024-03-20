834fbc0f73fd68b70839e84fa6f5865ac06efbab
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-03-20 15:32:11 +0530

    Time: 188 ms (31.90%), Space: 12.4 MB (61.78%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
+    int dp[1001][1001];
     int fun(string &s,int i,int j)
     {
         if(i>=j)
         {
             return 1;
         }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
         if(s[i]==s[j])
         {
-            return fun(s,i+1,j-1);
+            return dp[i][j]=fun(s,i+1,j-1);
         }
-        return 0;
+        return dp[i][j]=0;
     }
     string longestPalindrome(string s) {
         int maxlen=INT_MIN;
         int n=s.length();
+        memset(dp,-1,sizeof(dp));
         int st=0;
         for(int i=0;i<n;i++)
         {
@@ -22,7 +28,7 @@ public:
             {
                 if(fun(s,i,j)==true)
                 {
-                    if(j-i+1>=maxlen)
+                    if(j-i+1>maxlen)
                     {
                         maxlen=j-i+1;
                         st=i;
