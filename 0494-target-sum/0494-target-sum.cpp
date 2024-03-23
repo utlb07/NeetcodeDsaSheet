80574fa65afa9bfd51ef1990425b64b737aa094e
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-03-23 07:27:53 +0530

    Time: 803 ms (18.75%), Space: 10.5 MB (99.30%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int check(int index, int target, vector<int> &nums)
+    {
+        if(index<0 &&  target==0) return 1;
+        if(index<0) return 0;
+        int add=check(index-1,target-nums[index],nums);
+        int sub=check(index-1,target+nums[index],nums);
+
+        return add+sub;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+        return check (n-1,target,nums);
+    }
+};
