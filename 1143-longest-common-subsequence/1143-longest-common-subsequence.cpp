0e848cab6feab94777a46bc1ee29b9d64dc9ed52
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-03-23 07:50:21 +0530

    Time: 21 ms (60.33%), Space: 11.8 MB (82.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
     int dp[1001][1001];
-    int fun(int i,int j,string &s1, string &s2)
-    {
-        if(i<0||j<0)
-        {
-            return 0;
+    // int fun(int i,int j,string &s1, string &s2)
+    // {
+        // if(i<0||j<0)
+        // {
+        //     return 0;
+        // }
+        // if(dp[i][j]!=-1)
+        // {
+        //     return dp[i][j];
+        // }
+        // int pick=0;
+        // if(s1[i]==s2[j])
+        // {
+        //     pick=1+fun(i-1,j-1,s1,s2);
+        // }
+        // int notpick=max(fun(i-1,j,s1,s2),fun(i,j-1,s1,s2));
+        // return dp[i][j]=max(pick,notpick);
+    //}
+      int fun(string &s1, string &s2){
+         for(int i=0;i<s1.length()+1;i++){
+            for(int j=0;j<s2.length()+1;j++){
+                if(i==0||j==0){
+                    dp[i][j]=0;
+                }
+            }
         }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
+        for(int i=1;i<s1.length()+1;i++){
+            for(int j=1;j<s2.length()+1;j++){
+                if(s1[i-1]==s2[j-1]){
+                     dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
-        int pick=0;
-        if(s1[i]==s2[j])
-        {
-            pick=1+fun(i-1,j-1,s1,s2);
-        }
-        int notpick=max(fun(i-1,j,s1,s2),fun(i,j-1,s1,s2));
-        return dp[i][j]=max(pick,notpick);
+        return dp[s1.length()][s2.length()];
     }
     int longestCommonSubsequence(string text1, string text2) {
         memset(dp,-1,sizeof(dp));
-        return fun(text1.length()-1,text2.length()-1,text1,text2);
+        //return fun(text1.length()-1,text2.length()-1,text1,text2);
+        return fun(text1,text2);
     }
 };
