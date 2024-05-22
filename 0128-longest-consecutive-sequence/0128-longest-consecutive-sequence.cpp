748c002e03dd6cb24bc98bfe4138a0ebdcfa2d68
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-05-22 21:38:05 +0530

    Time: 873 ms (9.26%), Space: 74.1 MB (27.36%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,33 +1,35 @@
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        int n=nums.size();
-        if(n==0)
+class Solution
+{
+    public:
+        int longestConsecutive(vector<int> &nums)
         {
-            return 0;
-        }
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++)
-        {
-            mp[nums[i]]++;
-        }
-        int ans=1;
-        for(int i=0;i<n;i++)
-        {
-            if(mp.count(nums[i]-1))
+            int n = nums.size();
+            if (n == 0)
             {
-                continue;
+                return 0;
             }
-            int x=nums[i];
-            cout<<x<<endl;
-            int cnt=0;
-            while(mp.count(x))
+            unordered_map<int, int> mp;
+            for (int i = 0; i < n; i++)
             {
-                cnt++;
-                x++;
-                ans=max(cnt,ans);
+                mp[nums[i]]++;
             }
+            int ans = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if (mp.count(nums[i] - 1))
+                {
+                    continue;
+                }
+                int x = nums[i];
+                cout << x << endl;
+                int cnt = 0;
+                while (mp.count(x))
+                {
+                    cnt++;
+                    x++;
+                    ans = max(cnt, ans);
+                }
+            }
+            return ans;
         }
-        return ans;
-    }
 };
