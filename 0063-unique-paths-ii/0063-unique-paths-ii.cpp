6ef961f217c86d3b58de88d54fb95630a969360b
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-05-30 09:54:00 +0530

    Time: 6 ms (19.53%), Space: 10.2 MB (60.34%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,12 +1,13 @@
-class Solution {
-public:
-     int fun(int row, int col, vector<vector < int>> &dp, int m, int n,vector<vector<int>>& grid)
+class Solution
+{
+    public:
+        int fun(int row, int col, vector<vector < int>> &dp, int m, int n, vector< vector< int>> &grid)
         {
-            if (row >= m || col >= n||grid[row][col]==1)
+            if (row >= m || col >= n || grid[row][col] == 1)
             {
                 return 0;
             }
-            if (row == m - 1 && col == n - 1&&grid[row][col]!=1)
+            if (row == m - 1 && col == n - 1 && grid[row][col] != 1)
             {
                 return 1;
             }
@@ -14,36 +15,66 @@ public:
             {
                 return dp[row][col];
             }
-        //    int up = 0, right = 0;
-           
-                int up = fun(row + 1, col, dp, m, n,grid);
-          //  if (col + 1 <= n - 1)
-                int right = fun(row, col + 1, dp, m, n,grid);
+            int up = fun(row + 1, col, dp, m, n, grid);
+            int right = fun(row, col + 1, dp, m, n, grid);
             return dp[row][col] = up + right;
         }
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-          int m=grid.size();
-        int n=grid[0].size();
-          vector<vector < int>> dp(m + 1, vector<int> (n + 1, -1));
-      
-       	return fun(0,0,dp,m,n,grid);
-//         for (int i = 0; i < m; i++)
-//         {
-//            dp[i][0] = 1;
-//         }
-//         for (int j = 0; j < n; j++)
-//         {
-//             dp[0][j] = 1;
-//         }
+    int uniquePathsWithObstacles(vector<vector < int>> &grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector < int>> dp(m + 1, vector<int> (n + 1, 0));
+        if(grid[0][0]==1)
+        {
+            return 0;
+        }
+       		//return fun(0,0,dp,m,n,grid);
+    
+        // for (int i = 0; i < m; i++)
+        // {
+        //     if (grid[i][0]==0)
+        //     {
+        //         dp[i][0] = 1;
+        //     }
+        //     else
+        //     {
+        //         dp[i][0] = 0;
+        //     }
+        // }
+        // for (int j = 0; j < n; j++)
+        // {
+        //     if (grid[0][j]== 0)
+        //     {
+        //         dp[0][j] = 1;
+        //     }
+        //     else
+        //     {
+        //         dp[0][j] = 0;
+        //     }
+        // }
 
-//         int up = 0, right = 0;
-//         for (int i = 1; i < m; i++)
-//         {
-//             for (int j = 1; j < n; j++)
-//             {
-//                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-//             }
-//         }
-//         return dp[m - 1][n - 1];
+     //   int up = 0, right = 0;
+        dp[0][0]=1;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                 if(i==0 && j==0) continue;
+                 if(grid[i][j]==0){
+                    int up=0;
+                    int left=0;
+                    
+                    if(i-1>=0){
+                        up=dp[i-1][j];
+                    }
+                    if(j-1>=0){
+                        left=dp[i][j-1];
+                    }
+                      dp[i][j]=up+left;
+                 }
+                    
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
