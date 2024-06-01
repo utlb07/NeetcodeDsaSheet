5305376c97882cf3f46fff6a5475731e5453522e
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-06-01 12:16:38 +0530

    Time: 12 ms (45.11%), Space: 19.7 MB (52.16%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,37 +1,20 @@
-class Solution
-{
-    public:
-        int dp[301][5001];
-    int fun(vector<int> &coins, int id, int amount)
-    {
-
-        if (id == 0)
-        {
-            if (amount % coins[0] == 0)
-            {
-                return 1;
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+          int n=coins.size();
+        vector<vector<int>>dp(n, vector<int>(amount+1, 0));
+        for(int i=0; i<=amount; i++){
+            dp[0][i]=(i%coins[0]==0);
+        }
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=amount; j++){
+                int nt=dp[i-1][j], tk=0;
+                if(j>=coins[i]){
+                    tk=dp[i][j-coins[i]];
+                }
+                dp[i][j]=tk+nt;
             }
-            else
-            {
-                return 0;
-            }
         }
-        if (dp[id][amount] != -1)
-        {
-            return dp[id][amount];
-        }
-        int pick = 0;
-        if (amount - coins[id] >= 0)
-        {
-            pick = fun(coins, id, amount - coins[id]);
-        }
-        int notpick = fun(coins, id - 1, amount);
-        return dp[id][amount] = pick + notpick;
-    }
-    int change(int amount, vector<int> &coins)
-    {
-        int n = coins.size();
-        memset(dp, -1, sizeof(dp));
-        return (fun(coins, n - 1, amount));
+        return dp[n-1][amount];
     }
 };
