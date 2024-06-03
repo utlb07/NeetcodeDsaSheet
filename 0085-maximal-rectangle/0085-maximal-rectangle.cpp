2d906ff37dfb5ee04e77a39f491b38947753da61
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-06-03 18:16:29 +0530

    Time: 36 ms (38.30%), Space: 23.4 MB (5.99%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+vector<int> previous_smaller(vector<int>& heights){
+        vector<int> ans;
+        stack<int>st;
+        int n = heights.size();
+        
+        for(int i=0; i<n; i++){
+            while(st.empty() == false && heights[st.top()] >= heights[i] ){
+                st.pop();
+            }
+            int ele = (st.empty()) ? -1 : st.top();
+            ans.push_back(ele);
+            st.push(i);
+        }
+        return ans;
+    }
+
+    vector<int> next_smaller(vector<int>& heights){
+        vector<int> ans;
+        stack<int>st;
+        int n = heights.size();
+        
+        for(int i=n-1; i>=0; i--){
+            while(st.empty() == false && heights[st.top()] >= heights[i] ){
+                st.pop();
+            }
+            int ele = (st.empty()) ? n : st.top();
+            ans.push_back(ele);
+            st.push(i);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    
+    int largestRectangleArea(vector<int>& heights) {
+        int res = 0;
+        int n = heights.size();
+        vector<int>ps = previous_smaller(heights);
+        vector<int>ns = next_smaller(heights);
+        
+        for(int i=0; i<n; i++){
+            int curr = ( ns[i] - ps[i] -1 ) * heights[i];
+            res = max(res , curr);
+        }
+        return res;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+          int m=matrix.size();
+        if(m==0) return 0;
+        int n=matrix[0].size(), result=0;
+        vector<int> histogram(n, 0);
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]=='1')
+                    histogram[j]+=1;
+                else
+                    histogram[j]=0;
+            }
+            result = max(result, largestRectangleArea(histogram));
+        }
+        return result;
+        
+    }
+};
