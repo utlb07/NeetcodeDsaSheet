7cad7c98448fcc9df14d737960754a1dc1d1c770
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-06-03 08:43:08 +0530

    Time: 33 ms (32.06%), Space: 11.9 MB (78.98%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,47 +1,31 @@
 class Solution {
 public:
     int dp[1001][1001];
-    // int fun(int i,int j,string &s1, string &s2)
-    // {
-        // if(i<0||j<0)
-        // {
-        //     return 0;
-        // }
-        // if(dp[i][j]!=-1)
-        // {
-        //     return dp[i][j];
-        // }
-        // int pick=0;
-        // if(s1[i]==s2[j])
-        // {
-        //     pick=1+fun(i-1,j-1,s1,s2);
-        // }
-        // int notpick=max(fun(i-1,j,s1,s2),fun(i,j-1,s1,s2));
-        // return dp[i][j]=max(pick,notpick);
-    //}
-      int fun(string &s1, string &s2){
-         for(int i=0;i<s1.length()+1;i++){
-            for(int j=0;j<s2.length()+1;j++){
-                if(i==0||j==0){
-                    dp[i][j]=0;
-                }
-            }
+    int fun(int i,int j,int n,int m,string &s, string &t)
+    {
+        if(i<0||j<0)
+        {
+            return 0;
         }
-        for(int i=1;i<s1.length()+1;i++){
-            for(int j=1;j<s2.length()+1;j++){
-                if(s1[i-1]==s2[j-1]){
-                     dp[i][j] = 1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
         }
-        return dp[s1.length()][s2.length()];
+        if(s[i]==t[j])
+        {
+            dp[i][j]=1+fun(i-1,j-1,n,m,s,t);
+        }
+        else
+        {
+            dp[i][j]=max(fun(i-1,j,n,m,s,t),fun(i,j-1,n,m,s,t));
+        }
+        return dp[i][j];
     }
-    int longestCommonSubsequence(string text1, string text2) {
+    int longestCommonSubsequence(string s, string t) {
+        int n=s.length();
+        int m=t.length();
         memset(dp,-1,sizeof(dp));
-        //return fun(text1.length()-1,text2.length()-1,text1,text2);
-        return fun(text1,text2);
+        return fun(n-1,m-1,n,m,s,t);
+        
     }
 };
