2150067993454ead1f1bbd0377a1ae8389b900d0
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-06-05 10:57:05 +0530

    Time: 2 ms (92.60%), Space: 11.9 MB (70.78%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -25,8 +25,47 @@ class Solution
             return word2.length();
         if(word2.length()==0)
             return word1.length();
-            int i=0,j=0;
-            vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length()+1,-1));
-        return topDown(word1,word2,i,j,dp);
+          //  int i=0,j=0;
+            vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length()+1,0));
+    for (int i = 0; i <= word1.length(); ++i) {
+        for (int j = 0; j <= word2.length(); ++j) {
+            if (i == 0)
+                dp[i][j] = j;
+            else if (j == 0)
+                dp[i][j] = i;
+            else if (word1[i - 1] == word2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1];
+            else
+                dp[i][j] = 1 + min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]});
+        }
     }
+    return dp[word1.length()][word2.length()];
+}
+
+        // for(int j=0;j<word1.length();j++)
+        // {
+        //     dp[word1.length()][j]=word2.length()-j;
+        // }
+        // for(int i=0;i<word2.length();i++)
+        // {
+        //     dp[i][word2.length()]=word1.length()-i;
+        // }
+        // for(int i=word1.length()-1;i>=0;i--)
+        // {
+        //     for(int j=word2.length()-1;j>=0;j--)
+        //     {
+        //         if(word1[i]==word2[j])
+        //         {
+        //             dp[i][j]=1+dp[i+1][j+1];
+        //         }
+        //         else
+        //         {
+        //             dp[i][j]=1+dp[i+1][j];
+        //             dp[i][j]=1+dp[i+1][j+1];
+        //             dp[i][j]=1+dp[i][j+1];
+        //         }
+        //     }
+        // }
+        // return dp[0][0];
+    
 };
