6fa99a78aaa7baf4561b7504f2a2a7a1b7b1f472
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-06-06 10:37:44 +0530

    Time: 0 ms (100.00%), Space: 15.7 MB (30.97%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -27,6 +27,7 @@ public:
         //return fun(prices,0,0);
          int n = prices.size();
        // vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        dp[n][0]=0;
         for (int ind=n-1; ind>=0; ind--) {
             for (int buy=0; buy<=1; buy++) {
                 if (buy) dp[ind][buy] = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1]);
