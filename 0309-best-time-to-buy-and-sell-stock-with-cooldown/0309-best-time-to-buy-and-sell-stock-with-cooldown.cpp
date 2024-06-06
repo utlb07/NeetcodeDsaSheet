cc609d36dd6d6d1ddd0811821ba344f41d41f362
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-06-06 12:25:04 +0530

    Time: 4 ms (54.11%), Space: 13.4 MB (86.92%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[5001][3];
+    int dp[5002][3];
     int fun(vector<int>& prices,int id,int buy)
     {
         if(id>=prices.size())
@@ -25,7 +25,21 @@ public:
        // return profit;
     }
     int maxProfit(vector<int>& prices) {
-        memset(dp,-1,sizeof(dp));
-        return fun(prices,0,0);
+        memset(dp,0,sizeof(dp));
+        //return fun(prices,0,0);
+        int n=prices.size();
+        dp[n][0]=dp[n][1]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+
+            for(int buy=0; buy<=1; buy++) {
+                if(buy) {
+                    dp[i][buy] = max(-prices[i] + dp[i + 1][0], 0 + dp[i + 1][1]);
+                }
+                else dp[i][buy] = max(prices[i] + dp[i + 2][1], 0 + dp[i + 1][0]);
+            }
+        }
+        return dp[0][1];
+        
     }
 };
