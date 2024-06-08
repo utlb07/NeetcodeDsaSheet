d55c1d75093aeb894c87c2c1f897f2c2cb984999
Utkarsh Dwivedi <111227628+utlb07@users.noreply.github.com>
2024-06-08 15:46:57 +0530

    Time: 25 ms (29.03%), Space: 19.9 MB (14.30%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -29,8 +29,9 @@ public:
          vector<int> adj[n+1];
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(isConnected[i][j]==1){
+                if(isConnected[i][j]==1&&i!=j){
                     adj[i+1].push_back(j+1);
+                    adj[j+1].push_back(i+1);
                 }
             }
         }
